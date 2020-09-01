79339b826f5635669e2ba592fa4a7560dc0c7933
Leslie Joe <joe1989@live.com>
2020-09-01 14:56:23 +0000

    Add Font Awesome packages and update package files

---
+++ eventui/src/components/Beverage.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Beverage = () => (
+    <div>
+      <FontAwesomeIcon icon="check-square" />
+      <p>Your <FontAwesomeIcon icon="coffee" /> is hot and ready!</p>
+    </div>
+)
+
+export default Beverage;
