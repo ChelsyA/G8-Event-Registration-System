dc72e11ec6118399fba700e88c6705f08921e2cd
Leslie Joe <joe1989@live.com>
2020-09-02 19:31:31 +0000

    Modify sign up and login UI pages

--- eventui/src/App.js
+++ eventui/src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Auxiliary from './hoc/Auxiliary';
-import Navbar from './components/Navbar/Navbar';
+// import Navbar from './components/Navbar/Navbar';
 import FormPanel from './components/Form/Form';
 
-function App() {
-  return (
-    <Auxiliary>
-      <Navbar />
-      <FormPanel/>
-    </Auxiliary>
-  );
+class App extends Component {
+  state = {
+    isLogin: true
+  }
+
+  setSwitchForm = (islogin) => {
+    // console.log(islogin)
+    this.setState({isLogin: islogin});
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        {/* <Navbar /> */}
+        <div className="container mt-2 mb-3">
+          <div className="row mx-1">
+              <div className="col-md-9 col-sm-12 col-xs-12 mx-auto text-center py-3">
+                <div className="btn-group" role="group" aria-label="Switch button form">
+                  <button className="btn btn-primary" onClick={() => this.setSwitchForm(true)}>LOGIN</button>
+                  <button className="btn btn-danger" onClick={() => this.setSwitchForm(false)}>REGISTER</button>
+                </div>
+              </div>
+              <FormPanel isLogin={this.state.isLogin}/>
+          </div>
+        </div>
+      </Auxiliary>
+    );
+  }
 }
 
 export default App;
--- eventui/src/components/Form/Login/Login.js
+++ eventui/src/components/Form/Login/Login.js
@@ -4,30 +4,49 @@ import Auxiliary from "../../../hoc/Auxiliary";
 const FormPanel = (props) => {
   return (
     <Auxiliary>
-      <div className="container mt-5">
-        <div className="row mx-1">
-            <div className="col-md-4 col-sm-8 col-xs-6 mx-auto panel">
-                <div className="text-center">
-                    <h3 className="display-4">LOGIN</h3>
-                </div>
-                <form method="POST" className="p-1">
-                    <div className="form-group">
-                      <label htmlFor="email">Email address</label>
-                      <input type="email" className="form-control inputBG" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
-                      <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="password">Password</label>
-                      <input type="password" className="form-control inputBG" id="password" placeholder="Password"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-block borderRadius">Submit</button>
-                    <div className="mt-3 text-center">
-                        <p>Don't have an account? <a href="#/" className="link">Sign up!</a></p>
-                    </div>
-                </form>
-            </div>
+      <div className="col-md-4 col-sm-8 col-xs-6 mx-auto panel">
+        <div className="text-center py-4">
+          <h3 className="form-title font-weight-bold">LOGIN</h3>
         </div>
-    </div>
+        <form method="POST" className="p-1">
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className="form-control inputBG"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control inputBG"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary btn-block borderRadius"
+          >
+            Submit
+          </button>
+          <div className="mt-3 text-center">
+            <p>
+              Don't have an account?{" "}
+              <a href="#/" className="link">
+                Sign up!
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </Auxiliary>
   );
 };
