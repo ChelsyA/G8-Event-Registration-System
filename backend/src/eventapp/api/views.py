841e0f094a9a3ddd833ea454d862714ac1e0d8c2
Leslie Joe <joe1989@live.com>
2020-09-05 17:56:58 +0000

    Add registration and login functionalities to frontend

--- backend/src/eventapp/api/views.py
+++ backend/src/eventapp/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
-from validate_email import validate_email
 
 from eventapp.api.serializers import UserRegistrationSerializer
 from eventapp.models import User
@@ -12,8 +11,8 @@ def registration_view(request):
     if request.method == 'POST':
         data = {}
         email = request.data.get('email', '0').lower()
-        if validate_email(email) == None:
-            data['error_message'] = 'That email is already in use. sdcsdc'
+        if checkEmail(email) != None:
+            data['error_message'] = 'That email is already in use.'
             data['response'] = 'Error'
             return Response(data)
         
@@ -25,19 +24,24 @@ def registration_view(request):
         
         serializer = UserRegistrationSerializer(data=request.data)
         
+        password = request.data.get('password')
+        password2 = request.data.get('password2')
+        if password != password2:
+            return Response({
+                'Password': 'Password must match!'
+            })
+            
         if serializer.is_valid():
             user = serializer.save()
-            data['response'] = 'successfully registered new user.'
             data['email'] = user.email
             data['username'] = user.username
-            data['pk'] = user.pk
+            data['userId'] = user.pk
+            data['is_success'] = True
             # token = Token.objects.get(user=user).key
             # data['token'] = token
         else:
             data = serializer.errors
-        return Response({
-            'is_success': True,
-        })
+        return Response(data)
 
 def validate_username(username):
     user = None
@@ -47,3 +51,12 @@ def validate_username(username):
         return None
     if user != None:
         return username
+
+def checkEmail(email):
+    user = None
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return None
+    if user != None:
+        return email
--- backend/src/eventproject/urls.py
+++ backend/src/eventproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
+from eventapp.views import test
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/eventapp/', include('eventapp.api.urls', 'eventapp_api')),
