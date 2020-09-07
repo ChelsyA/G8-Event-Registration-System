dca6c41c114b59e41ded1fc8d4d12197c4d5f48b
Leslie Joe <joe1989@live.com>
2020-09-07 19:18:25 +0000

    Add build folder from frontend app for production

---
+++ backend/src/eventui/build/precache-manifest.09708185b1494974eaf9d5d44756b391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abcde8c11d7c687f05ca5ab706e0040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf71d973bd0cf915310",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "71ce6dbb6c002f6fdeb7",
+    "url": "/static/css/main.55881635.chunk.css"
+  },
+  {
+    "revision": "faf71d973bd0cf915310",
+    "url": "/static/js/2.ff3740f8.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/static/js/2.ff3740f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ce6dbb6c002f6fdeb7",
+    "url": "/static/js/main.40d94d8e.chunk.js"
+  },
+  {
+    "revision": "1fe6ce7e560f4c594b2d",
+    "url": "/static/js/runtime-main.518b0c19.js"
+  }
+]);
