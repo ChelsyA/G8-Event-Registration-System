ff9cfd523c754f06dcecf75ae70a5e28affdf9e9
Leslie Joe <joe1989@live.com>
2020-09-12 08:34:11 +0000

    Fix bugs

--- src/eventapp/api/urls.py
+++ src/eventapp/api/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
-from eventapp.api.views import register_view, UserListView, UserRetrieveUpdateDestroy
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from eventapp.api.views import register_view, UserListView, UserRetrieveUpdateDestroy, EventView
+
+router = DefaultRouter()
+router.register('events', EventView, basename='events')
+
 
 app_name = 'eventapp'
 
 urlpatterns = [
-	path('users', UserListView.as_view(), name="users"),
+    path('events/', include(router.urls)),
+	path('users/', UserListView.as_view(), name="users"),
 	path('users/register', register_view, name="user_register"),
 	path('users/<int:pk>/', UserRetrieveUpdateDestroy.as_view(), name="users"),
 ]
