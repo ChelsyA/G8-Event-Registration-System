7cb04f46763000ce3edce3e09a69b1e445d431f0
Leslie Joe <joe1989@live.com>
2020-09-18 03:45:40 +0000

    Fix bugs

--- src/eventproject/urls.py
+++ src/eventproject/urls.py
@@ -4,19 +4,18 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 from eventapp.views import (add_user_to_event, eventbookings,
-                            remove_user_from_event, test, userbookings)
+                            remove_user_from_event, userbookings)
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/', include('eventapp.api.urls', namespace="eventapp")),
-    path('test/<int:eid>/', test),
-    path('userbookings/<int:id>/', userbookings, name="userbookings"),
-    path('eventbookings/<int:id>/', eventbookings, name="eventbookings"),
-    path('adduserevent/<int:ui>/<int:ei>/',
-         add_user_to_event, name="add_user_to_event"),
-    path('removeuserevent/<int:ui>/<int:ei>/',
-         remove_user_from_event, name="remove_user_from_event"),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+#     path('userbookings/<int:id>/', userbookings, name="userbookings"),
+#     path('eventbookings/<int:id>/', eventbookings, name="eventbookings"),
+#     path('adduserevent/<int:ui>/<int:ei>/',
+#          add_user_to_event, name="add_user_to_event"),
+#     path('removeuserevent/<int:ui>/<int:ei>/',
+#          remove_user_from_event, name="remove_user_from_event"),
 ]
