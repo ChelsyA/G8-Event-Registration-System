d26e90cfebb45fbecdd6e5e81276991bd8862a2c
Leslie Joe <joe1989@live.com>
2020-09-18 00:22:55 +0000

    Fix bugs

---
+++ src/eventui/build/precache-manifest.db03dc974edfa6ca0041df1b4ae5e24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e069c40793e0806d9d8c82cf9d33c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0042e81d2c61d9bdc2e3",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6362ce073ecb39041923",
+    "url": "/static/css/main.1163eb84.chunk.css"
+  },
+  {
+    "revision": "0042e81d2c61d9bdc2e3",
+    "url": "/static/js/2.bb5d6254.chunk.js"
+  },
+  {
+    "revision": "95476cf5e78c81d928d037b2947b5eb6",
+    "url": "/static/js/2.bb5d6254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6362ce073ecb39041923",
+    "url": "/static/js/main.ca68235a.chunk.js"
+  },
+  {
+    "revision": "1fe6ce7e560f4c594b2d",
+    "url": "/static/js/runtime-main.518b0c19.js"
+  }
+]);
