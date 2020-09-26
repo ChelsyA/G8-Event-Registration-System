9c300ec5284e6a08ea2661809d6aae1f722f7a25
Leslie Joe <leslie.kojo.okyere@outlook.com>
2020-09-26 11:22:30 +0000

    Add comments to each functionality and build for production

---
+++ src/eventui/build/precache-manifest.394d97913d865932253a05d60e1a1422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e4d273472fb7902fa308794fa185f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "238a9a6bb0b69ecccd2a",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "8c8b6492f539449d61f4",
+    "url": "/static/css/main.1163eb84.chunk.css"
+  },
+  {
+    "revision": "238a9a6bb0b69ecccd2a",
+    "url": "/static/js/2.ff379097.chunk.js"
+  },
+  {
+    "revision": "4afb09862cbd8ec602c544e5730820df",
+    "url": "/static/js/2.ff379097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8b6492f539449d61f4",
+    "url": "/static/js/main.fa1f61aa.chunk.js"
+  },
+  {
+    "revision": "1fe6ce7e560f4c594b2d",
+    "url": "/static/js/runtime-main.518b0c19.js"
+  }
+]);
